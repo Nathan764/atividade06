4c4f25bef94fbdcefe444a938601662de0e4ce10
Nathan764 <nathanoli330@gmail.com>
2022-09-12 21:02:03 -0300

    atividade06

---
+++ script.js
@@ -0,0 +1,32 @@
+$(document).ready(function () {
+
+    $("#conteudo").hide();
+
+    $("#link_abre").click(function (){
+
+    $("#conteudo").slideToggle("slow");
+
+    })
+})
+
+$(document).ready(function () {
+
+    $("#conteudo1").hide();
+
+    $("#link_abre1").click(function (){
+
+    $("#conteudo1").slideToggle("slow");
+
+    })
+})
+
+$(document).ready(function () {
+
+    $("#conteudo2").hide();
+
+    $("#link_abre2").click(function (){
+
+    $("#conteudo2").slideToggle("slow");
+
+    })
+})
